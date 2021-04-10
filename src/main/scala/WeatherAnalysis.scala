1f14ba85a395806b596de8094c906ef600f54d64
BrendanMolin <brendan.molin@vt.edu>
2021-04-10 12:44:39 -0400

    create pseudo code for project plan, not working

--- src/main/scala/WeatherAnalysis.scala
+++ src/main/scala/WeatherAnalysis.scala
@@ -9,10 +9,6 @@ import org.apache.spark.{SparkContext, SparkConf}
  */
 object WeatherAnalysis {
 
-  class SymInt(value: Int, p: Int)
-  class SymFloat(value: Float, p: Int)
-
-
   def main(args: Array[String]) {
     try {
       //set up logging
@@ -41,37 +37,31 @@ object WeatherAnalysis {
       val ctx = new SparkContext(sparkConf)
       //val lines = ctx.textFile(logFile, 1)
       val lines = ctx.textFile("./data/all_data", 1)
-      for (l <- lines.take(10)) {
-        //list = o._2 :: list
-        println(l)
-      }
-      logger.info("Is lines variable empty? " + lines.isEmpty())
-      logger.info("Example entry in lines: " + lines.first())
       val split = lines.flatMap{s =>
         val tokens = s.split(",")
+        val lineHis = List()
         // finds the state for a zipcode
         var state = zipToState(tokens(0))
         var date = tokens(1)
         // gets snow value and converts it into millimeter
-        val snow = convert_to_mm(tokens(2))
+        val snowTuple = convert_to_mm(tokens(2)) // CAPTURE HERE
+        lineHis.append(snowTuple._1)
         //gets year
         val year = date.substring(date.lastIndexOf("/"))
         // gets month / date
         val monthdate= date.substring(0,date.lastIndexOf("/")-1)
         List[((String , String) , Float)](
-          ((state , monthdate) , snow) ,
-          ((state , year)  , snow)
+          ((state , monthdate) , snow) , // CAPTURE HERE (PROBLEM HERE: HOW TO RETURN LINE NUMBER WITH VALUE??? USE SYM PROBABLY
+          ((state , year)  , snow) // CAPTURE HERE
         ).iterator
       }
-
-      for (fm <- split.take(10)) {
-        //list = o._2 :: list
-        println(fm)
-      }
       val deltaSnow = split.groupByKey().map{ s  =>
-        val delta =  s._2.max - s._2.min
+        val s1 = s._2
+        val s2 = s._2
+        val delta =  s1.max - s2.min
         (s._1 , delta)
       }//.filter(s => WeatherAnalysis.failure(s._2))
+      //(key, (snow, (lines hit 1, lines hit 2)))
       val output = deltaSnow.collect()
       logger.info("Testing...")
       var list = List[Long]()
@@ -83,12 +73,12 @@ object WeatherAnalysis {
     }
   }
 
-  def convert_to_mm(s: String): Float = {
+  def convert_to_mm(s: String): (Float, Int) = {
     val unit = s.substring(s.length - 2)
     val v = s.substring(0, s.length - 2).toFloat
     unit match {
-      case "mm" => return v
-      case _ => return v * 304.8f
+      case "mm" => return (v, 80) // now record has v, 80
+      case _ => return v * 304.8f, 81) // now record has v, 81
     }
   }
   def failure(record:Float): Boolean ={
