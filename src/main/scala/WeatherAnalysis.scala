7aaba34841034f63c8c3c45eeac58eeb6997df31
ehmartens <65190020+ehmartens@users.noreply.github.com>
2021-04-21 18:38:00 -0400

    First pass at adding logic, not yet working

--- src/main/scala/WeatherAnalysis.scala
+++ src/main/scala/WeatherAnalysis.scala
@@ -22,8 +22,6 @@ object WeatherAnalysis {
 
       val ctx = new SparkContext(sparkConf)
       val lines = ctx.textFile("./data/all_data", 1)
-      println("Is lines variable empty? " + lines.isEmpty())
-      println("Example entry in lines: " + lines.first())
       val split = lines.flatMap{s =>
         var covS = CovString(s, ArrayBuffer[Int]())
         val covtokens = covS.split(",")
@@ -50,7 +48,7 @@ object WeatherAnalysis {
         val s2 = s._2
         val delta =  s1.max - s2.min
         (s._1 , delta)
-      }//.filter(s => WeatherAnalysis.failure(s._2))
+      }
 
       val output = deltaSnow.collect()
       var list = List[Long]()
@@ -58,6 +56,14 @@ object WeatherAnalysis {
         println(o)
       }
       println(detectFailedLines(output))
+
+      val failLinesOutput = summarizeFailLines(output)
+      val failLineOutputMap = failLinesOutput._1
+      val totalNumberOfPasses = failLinesOutput._2
+      val totalNumberOfFailures = failLinesOutput._3
+      println("totalNumberOfFailures: " + totalNumberOfFailures)
+
+      print(getFailedLine(failLineOutputMap, totalNumberOfPasses, totalNumberOfFailures))
     }
   }
 
@@ -92,6 +98,60 @@ object WeatherAnalysis {
       }
     }
     return resultMap
+  }
+
+  def summarizeFailLines(resultList:Array[((String, String), CovFloat)]): (collection.mutable.HashMap[Int, (Int, Int)], Int, Int) = {
+    // (lineNo, (No of passing records, No of failing records)
+    var totalNumberOfPasses = resultList.length
+    var totalNumberOfFailures = 0
+    val resultMap = collection.mutable.HashMap[Int, (Int, Int)]() // Create new empty Map
+    for (o <- resultList){
+      val isFailure = failure(o._2.value)
+      if (isFailure){
+        totalNumberOfFailures += 1
+        totalNumberOfPasses -= 1
+      }
+      for (eaLine <- o._2.hist){
+        if (!resultMap.contains(eaLine)){ // If this line No isn't in the dictionary yet, add it
+          resultMap.+=((eaLine, (0, 0)))
+        }
+        if (isFailure){
+          val rec = resultMap.get(eaLine)
+          val new_rec = (rec.get._1, 1 + rec.get._2)
+          resultMap.update(eaLine, new_rec) // tuple can be stored in a separate var if error returned
+        } else {
+          val rec = resultMap.get(eaLine)
+          val new_rec = (1 + rec.get._1, rec.get._2)
+          resultMap.update(eaLine, new_rec) // tuple can be stored in a separate var if error returned
+        }
+      }
+    }
+    return (resultMap, totalNumberOfPasses, totalNumberOfFailures)
+  }
+
+  def getFailedLine(resultMap:collection.mutable.HashMap[Int, (Int, Int)], totalNumberOfPasses:Int, totalNumberOfFailures:Int): Int = {
+    /* Takes the result map of (lineNo, (passing cases, failing cases))
+        and returns the line number most likely to be causing the issue.
+        Returns -1 if no failures were detected.
+     */
+    if (totalNumberOfFailures == 0){
+      return -1 /* No failing lines */
+    }
+    //val LineRankings = collection.mutable.HashMap[Int, Int]() // Create new empty Map
+    var lineRankings = ArrayBuffer[(Int, Double)]() //Create new list of Tuples
+    for (eaLine <- resultMap){
+      val lineNo = eaLine._1
+      val failScore = eaLine._2._2 / totalNumberOfFailures
+      val passScore = eaLine._2._1 / totalNumberOfPasses
+      val score = failScore / (failScore + passScore)
+      //LineRankings(lineNo) = score
+      lineRankings.append((lineNo, score))
+    }
+    // Sort Array
+    lineRankings.sortBy(_._2)(Ordering[Double].reverse)
+    println(lineRankings.take(10))
+
+    return lineRankings(0)._1
   }
 
   def failure(record:Float): Boolean ={
